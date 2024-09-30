f9b63bb50caf318d481ce0a2c2d92a4d59d29378
nwabueze1 <padrefidelis111@gmail.com>
2024-09-30 06:23:11 +0100

    initial commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,144 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={
+                "https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_9V5awgZYH5dajR2MyWHgQsaFAuiT%26teamId%3Dnwabueze1s-projects%26withStatus%3D1&w=640&q=75&dpl=dpl_CRwQXRLbaFXypKYRYQFMGgGeJSVs"
+              }
+              isBlog={false}
+              title="Hivedeck"
+              description="A website builder platform that allows users to  create thier website in minutes, without writing code. The platform has pre-built templates which can be customized to build website pages."
+              ghLink="https://github.com/nwabueze1/ease-of-build"
+              demoLink="https://easy-of-dev.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={
+                "https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_4UMpfNZTCcJQjmFybFb7ucNtfwpY%26teamId%3Dnwabueze1s-projects%26withStatus%3D1&w=640&q=75&dpl=dpl_CRwQXRLbaFXypKYRYQFMGgGeJSVs"
+              }
+              isBlog={false}
+              title="Kiamoni Template"
+              description="An e-commerce website template that is blog enabled and build with react.js, Typescript, Material-UI, and graphql. Have features which allows user of hivedeck platform to use and customize this template to create webpages."
+              ghLink="https://github.com/nwabueze1/temp-onikam"
+              demoLink="https://sonikan.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={
+                "https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_7qoq77HuLhYGpAdei8qFfeCqvo3d%26teamId%3Dnwabueze1s-projects%26withStatus%3D1&w=640&q=75&dpl=dpl_CRwQXRLbaFXypKYRYQFMGgGeJSVs"
+              }
+              isBlog={false}
+              title="Elegante Template"
+              description="An e-commerce website template that is blog enabled and build with react.js, Typescript, Material-UI, and graphql. Have features which allows user of hivedeck platform to use and customize this template to create webpages."
+              ghLink="https://github.com/nwabueze1/etalenge"
+              demoLink="https://template-eleg.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={
+                "https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_86QWPXwUvHxxRG9GgtASvyCq4Peo%26teamId%3Dnwabueze1s-projects%26withStatus%3D1&w=640&q=75&dpl=dpl_CRwQXRLbaFXypKYRYQFMGgGeJSVs"
+              }
+              isBlog={false}
+              title="Eat up Template"
+              description="An e-commerce website template that is blog enabled and build with react.js,Typescript, Material-UI, and graphql. Have features which allows user of hivedeck platform to use and customize this template to create webpages."
+              ghLink="https://github.com/nwabueze1/feating"
+              demoLink="https://template-eat-3276.vercel.app/"
+            />
+          </Col>
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Chatify"
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/soumyajit4419/Chatify"
+              demoLink="https://chatify-49.web.app/"
+            />
+          </Col> */}
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Fidelis Blog"
+              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              ghLink="https://github.com/nwabueze1/fidelis-blog"
+              demoLink="https://fidelis-blog.vercel.app/"
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Editor.io"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://editor.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
